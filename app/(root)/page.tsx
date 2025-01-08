81f49ff1edcba7204e9e761b39fbf295035adb47
mbanixox <46408125+mbanixox@users.noreply.github.com>
2025-01-09 00:03:43 +0300

    Implement server-side search functionality
    - Created a `SearchForm` component to handle server-side search using React Form
    - Added button components to submit or reset the search input on the client side
    - Enabled seamless integration between client-side UI and server-side search functionality

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,23 @@
-export default function Home() {
+import SearchForm from '../../components/SearchForm';
+
+export default async function Home( { searchParams }: {
+  searchParams: Promise<{ query?: string }>
+}) {
+
+  const query = (await searchParams).query;
+
   return (
     <>
-      <h1 className="text-2xl">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">Pitch Your Startup, <br />
+        Connect with Entrepreneurs</h1>
+
+        <p className="sub_heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed!</p>
+
+        <SearchForm query={query} />
+      </section>
+      
+      
     </>
   );
 }
