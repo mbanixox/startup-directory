11325da18e3d34c8c9822163fd3bd346009ba374
mbanixox <46408125+mbanixox@users.noreply.github.com>
2025-01-09 21:01:54 +0300

    Implement real-time updates and search functionality
    - Integrated Sanity Live Content API for real-time updates without page refresh
    - Enabled real-time search of startups through the search component
    - Improved user experience with dynamic and responsive data fetching

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
 import StartupCard, { StartupTypeCard } from '@/components/StartupCard';
 import SearchForm from '../../components/SearchForm';
-import { client } from '@/sanity/lib/client';
 import { STARTUPS_QUERY } from '@/sanity/lib/queries';
+import { sanityFetch, SanityLive } from '@/sanity/lib/live';
 
 
 export default async function Home( { searchParams }: {
@@ -9,8 +9,9 @@ export default async function Home( { searchParams }: {
 }) {
 
   const query = (await searchParams).query;
+  const params = { search: query || null};
 
-  const posts = await client.fetch(STARTUPS_QUERY);
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
   return (
     <>
@@ -40,6 +41,8 @@ export default async function Home( { searchParams }: {
         </ul>
 
       </section>
+
+      <SanityLive />
       
     </>
   );
