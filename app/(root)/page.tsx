46a03e004929d036e67696319f36cc9b62ca34f0
mbanixox <46408125+mbanixox@users.noreply.github.com>
2025-01-09 15:10:23 +0300

    Add reusable card component for startup summary
    - Created a card component to display startup summary information
    - Updated the home page to use the new card component
    - Updated the utils file to include a function to format the date
    - Updated the next config file to include the new component directory

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import StartupCard from '@/components/StartupCard';
 import SearchForm from '../../components/SearchForm';
 
 export default async function Home( { searchParams }: {
@@ -5,6 +6,19 @@ export default async function Home( { searchParams }: {
 }) {
 
   const query = (await searchParams).query;
+
+  const posts = [{
+    _createdAt: new Date(),
+    views: 55,
+    author: { _id: 1, name: "John Doe" },
+    _id: 1,
+    description: "This is a description",
+    image: "https://images.pexels.com/photos/256381/pexels-photo-256381.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    category: "Tech",
+    title: "Startup Title",
+
+
+  }];
 
   return (
     <>
@@ -17,6 +31,23 @@ export default async function Home( { searchParams }: {
         <SearchForm query={query} />
       </section>
       
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post}/>
+            ))
+          ) : (
+              <p className="no results">No startups found</p>
+          )}
+
+        </ul>
+
+      </section>
       
     </>
   );
